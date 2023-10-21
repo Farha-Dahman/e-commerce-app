4cd2fbe5a87ae07f34318f3ee1a8343ee2c80288
Farha Dahman <farhajehad@gmail.com>
2023-10-21 23:15:44 +0300

    adding the project startup

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import "dotenv/config.js";
+import initApp from "./src/modules/app.router.js";
+const app = express();
+const PORT = process.env.PORT || 5555;
+
+initApp(app,express);
+
+app.listen(PORT,()=>{
+    console.log(`server is running on ${PORT}`);
+})
