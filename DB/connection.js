6d9d5f6b0d6c8d4953d9899605f704db6284203e
Farha Dahman <farhajehad@gmail.com>
2023-11-04 14:22:32 +0200

    connecting to the DB and creating the user & category models

---
+++ DB/connection.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+const connectionDB = async()=>{
+    return await mongoose.connect(process.env.DB).then( ()=>{
+        console.log("connect successfully!");
+    }).catch( (err)=>{
+        console.log(`error when connect DB! ${err}`);
+    })
+}
+export default connectionDB;
