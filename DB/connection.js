68eba7c66cd25673a404cfdcfef1f54a3613932e
Farha Dahman <farhajehad@gmail.com>
2023-11-25 21:34:25 +0200

    update formating for the connection DB file

--- DB/connection.js
+++ DB/connection.js
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
-const connectionDB = async()=>{
-    return await mongoose.connect(process.env.DB).then( ()=>{
-        console.log("connect successfully!");
-    }).catch( (err)=>{
-        console.log(`error when connect DB! ${err}`);
+const connectionDB = async () => {
+  return await mongoose
+    .connect(process.env.DB)
+    .then(() => {
+      console.log("connect successfully!");
     })
-}
+    .catch((err) => {
+      console.log(`error when connect DB! ${err}`);
+    });
+};
 export default connectionDB;
